93a724f75bbadc4d4a6586ba51a8e6f5f3467224
stephen <stephenman@cherrypicks.com>
2018-09-27 23:42:03 +0800

    changed for reading DB credential from opsWorks if ENVIRONMENT exists in opsworks

---
+++ config/local.js
@@ -0,0 +1,10 @@
+module.exports = {
+	db 					        : 
+	{
+		host					: 'localhost',
+		port 					: 3306,
+  		database 				: 'photoalbums',
+		user     				: 'root',
+ 		password 				: 'cherrypicks'
+	}
+}
