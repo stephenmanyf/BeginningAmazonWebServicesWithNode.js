7fdc45b45f25e6db745d262d3686064eefa05ec1
stephen <stephenman@cherrypicks.com>
2018-10-07 23:59:32 +0800

    fixed indent

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var users = require('./routes/users');
 var photos = require('./routes/photos');
 var albums = require('./routes/albums');
 var globals = require('./lib/globals');
-var mysql       = require('mysql');
+var mysql = require('mysql');
 var app = express();
 
 // view engine setup
