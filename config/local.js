1607d31bb492c5ca300d8dcafcdbdc49946108dc
stephen <stephenman@cherrypicks.com>
2018-10-07 23:40:21 +0800

    Added S3 credential

--- config/local.js
+++ config/local.js
@@ -6,5 +6,10 @@ module.exports = {
   		database 				: 'photoalbums',
 		user     				: 'root',
  		password 				: 'cherrypicks'
+	},
+	awsVariables				: {
+		bucket					: 'photoalbums-cdn-s3',
+		key						: 'AKIAJWG3PXEJPFBFV6HA',
+		secret					: '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'
 	}
 }
